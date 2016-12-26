a7eab04c422a17027f7ec983905e8b44ef989113
danisluis6 <vuongluis@hotmail.com>
2016-12-26 11:23:34 +0700

    ok

--- AlertDialog/app/src/main/java/com/example/enclaveit/alertdialog/AlertDialog.java
+++ AlertDialog/app/src/main/java/com/example/enclaveit/alertdialog/AlertDialog.java
@@ -1,17 +1,8 @@
-package com.example.enclaveit.app_meida.lib;
+package com.example.enclaveit.alertdialog;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.enclaveit.alertdialog.R;
 
 /**
  * Created by enclaveit on 16/12/2016.
@@ -21,39 +12,26 @@ public class AlertDialog {
 
     private static android.app.AlertDialog alertDialog;
 
-    public static android.app.AlertDialog onCreateDialog(Activity activity, String titlex, String content, final Context context) {
+    public static android.app.AlertDialog onCreateDialog(final Activity activity, String titlex, String messagex, final Context context) {
         /**
          * Initial AlertDialog and draw componet concerning inside AlertDialog
          */
         android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(activity);
-        /**
-         * Get the layout inflater from user interface graphic
-         */
-        LayoutInflater inflater = activity.getLayoutInflater();
-        View layout = inflater.inflate(R.layout.ui_alert, (ViewGroup) activity.findViewById(R.id.toast_layout_root));
-        /**
-         * Inflate and set the layout for the dialog
-         * Pass null as the parent view because its going in the dialog layout
-         */
-        builder.setView(layout);
 
-        /// Init components TextView
-        ImageView image = (ImageView) layout.findViewById(R.id.alert);
-        TextView title = (TextView) layout.findViewById(R.id.title);
-        TextView text = (TextView) layout.findViewById(R.id.text);
+        // Set title for AlertDialog
+        builder.setTitle(titlex);
 
-        // Get data and set
-//        title.setText(titlex);
-//        text.setText(content);
-
+        // Set dialog message
+        builder.setMessage(messagex)
+        .setCancelable(false)
         // Add action buttons
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int id) {
-
+                activity.finish();
             }
-        });
-        builder.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+        })
+        .setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 dialog.cancel();
             }
--- AlertDialog/app/src/main/java/com/example/enclaveit/alertdialog/MainActivity.java
+++ AlertDialog/app/src/main/java/com/example/enclaveit/alertdialog/MainActivity.java
@@ -1,22 +1,26 @@
 package com.example.enclaveit.alertdialog;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.widget.RelativeLayout;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+    private Context context = this;
+    private Button demo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        (RelativeLayout)this.findViewById(R.id.activity_main).setOnClickListener(new View.OnClickListener() {
+        demo = (Button)this.findViewById(R.id.demo);
+        demo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                AlertDialog.onCreateDialog((Activity)context,"Your titles","Your messages",context).show();
             }
         });
     }
